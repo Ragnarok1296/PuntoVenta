c28c2be2a94a4d490c25c4ee80f796a18f0fa8d3
Ragnarok1296 <32114784+Ragnarok1296@users.noreply.github.com>
2018-07-16 12:26:23 -0500

    Plantilla

---
+++ PuntoVentaServidor/Modelos/MySQLConexion.cs
@@ -0,0 +1,94 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PuntoVentaServidor.Modelos
+{
+    public class MySQLConexion
+    {
+
+        private string server = "ragnarok-projects.mysql.database.azure.com";
+        private string user = "Ragnarok1296@ragnarok-projects";
+        private string password = "Ragnarok051296";
+        private string database = "puntoventa";
+        private MySqlConnection conn;
+
+
+        private MySqlCommand cmd;
+
+
+        public MySQLConexion() {
+            conn = new MySqlConnection("SERVER=" + server + "; DATABASE=" + database + "; UID=" + user + "; PASSWORD=" + password + ";");
+        }
+
+        //Verifica si se puede conectar a la base de datos
+        #region Conexion
+        public bool connection() {
+
+            bool conectado = false;
+
+            try {
+                conn.Open();
+                conectado = true;
+            } catch (Exception) {
+                conectado = false;
+            } finally {
+                conn.Close();
+            }
+
+            return conectado;
+
+        }
+        #endregion
+
+        //Este metodo devuelve un Datatable con la tabla seleccionada y los campos que le hayas mandado en el WebService
+        #region Consulta Busqueda
+        public DataTable consulta_busqueda(String query)
+        {
+            DataTable table = new DataTable();
+            
+            try {
+                conn.Open();
+                MySqlDataAdapter select = new MySqlDataAdapter(query, conn);
+                MySqlCommandBuilder commandBuilder = new MySqlCommandBuilder(select);
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                select.Fill(table);
+            } catch (Exception) {
+
+            } finally {
+                conn.Close();
+            }
+            
+            return table;
+
+        }
+        #endregion
+
+        //Este metodo devuelve un Boleano para saber si la operacion fue un exito, se le pasa como parametro el query con la operacion segun el WebService
+        #region Insertar Actualizar Eliminar
+        public bool insertar_actualizar_eliminar(string query)
+        {
+            bool operacion = false;
+            
+            try {
+                conn.Open();
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                operacion = true;
+            }
+            catch (Exception) {
+                operacion = false;
+            } finally {
+                conn.Close();
+            }
+            
+            return operacion;
+
+        }
+        #endregion
+
+    }
+}
--- PuntoVentaServidor/WebService/WSProveedores.asmx.cs
+++ PuntoVentaServidor/WebService/WSProveedores.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using PuntoVentaServidor.Modelos;
+using Newtonsoft.Json;
 
 namespace PuntoVentaServidor.WebService
 {
@@ -16,11 +18,125 @@ namespace PuntoVentaServidor.WebService
     // [System.Web.Script.Services.ScriptService]
     public class WSProveedores : System.Web.Services.WebService
     {
-        //Prueba 2
-        [WebMethod]
-        public string HelloWorld()
+        MySQLConexion conexion;
+        Proveedores proveedores;
+
+        // Instanciamos la conexion y al proveedor
+        public WSProveedores()
         {
-            return "Hola a todos";
+            conexion = new MySQLConexion();
+            proveedores = new Proveedores();
         }
+        
+        [WebMethod]
+        public String ConsultarProveedores()
+        {
+            string json = "";
+
+            // Verifico si la conexion es correcta
+            if (conexion.connection()) {
+                // Creo el query que mandare
+                String query = "call ConsultarProveedores()";
+
+                // Mando llamar el metodo de MYSQLConexion el cual me devuelve un datatable y lo serializo
+                json = JsonConvert.SerializeObject(conexion.consulta_busqueda(query));
+            }
+
+            //Retorno la cadena con formato json
+            return json;
+        }
+
+        [WebMethod]
+        public String InsertarProveedores(string json)
+        {
+            string operacion = "0";
+
+            //Desserializo la cadena que manda el cliente en formato json y lo convierto en el modelo que tengo de Provedores
+            proveedores = JsonConvert.DeserializeObject<Proveedores>(json);
+
+            // Verifico si la conexion es correcta
+            if (conexion.connection()) {
+
+                // Creo el query que mandare
+                String query = "call InsertarProveedor('" + proveedores.Id.ToString() + "','" + proveedores.RazonSocial + "','" 
+                    + proveedores.DireccionFiscal + "','" + proveedores.DireccionUbicacion + "','" 
+                    + proveedores.Rfc + "','" + proveedores.NombreContacto + "','" 
+                    + proveedores.Telefono + "','" + proveedores.Correo + "')";
+
+                //Mando llamar al metodo para inssertar el cual me devolvera un booleano y asi retorno un 1 si todo salio bien 
+                if (conexion.insertar_actualizar_eliminar(query))
+                    operacion = "1";
+            }
+
+            //Retorno un string con un 1 o 0 para saber si la operacion fue exitosa
+            return operacion;
+            
+        }
+
+        [WebMethod]
+        public String EliminarProveedores(string id)
+        {
+            string operacion = "0";
+
+            // Verifico si la conexion es correcta
+            if (conexion.connection()) {
+
+                // Creo el query que mandare
+                String query = "call EliminarProveedor('" + id + "')";
+
+                //Mando llamar al metodo para eliminar el cual me devolvera un booleano y asi retorno un 1 si todo salio bien 
+                if (conexion.insertar_actualizar_eliminar(query))
+                    operacion = "1";
+            }
+
+            //Retorno un string con un 1 o 0 para saber si la operacion fue exitosa
+            return operacion;
+        }
+
+        [WebMethod]
+        public String ActualizarProveedores(string json)
+        {
+            string operacion = "0";
+
+            //Desserializo la cadena que manda el cliente en formato json y lo convierto en el modelo que tengo de Provedores
+            proveedores = JsonConvert.DeserializeObject<Proveedores>(json);
+
+            // Verifico si la conexion es correcta
+            if (conexion.connection())
+            {
+
+                String query = "call ActualizarProveedor('" + proveedores.Id.ToString() + "','" + proveedores.RazonSocial + "','"
+                    + proveedores.DireccionFiscal + "','" + proveedores.DireccionUbicacion + "','"
+                    + proveedores.Rfc + "','" + proveedores.NombreContacto + "','"
+                    + proveedores.Telefono + "','" + proveedores.Correo + "')";
+
+                //Mando llamar al metodo para actualizar el cual me devolvera un booleano y asi retorno un 1 si todo salio bien 
+                if (conexion.insertar_actualizar_eliminar(query))
+                    operacion = "1";
+            }
+
+            //Retorno un string con un 1 o 0 para saber si la operacion fue exitosa
+            return operacion;
+        }
+
+        [WebMethod]
+        public String BuscarProveedores(string busqueda)
+        {
+            string json = "";
+
+            // Verifico si la conexion es correcta
+            if (conexion.connection()) {
+
+                // Creo el query que mandare
+                String query = "call BusquedaProveedores('" + busqueda + "')";
+
+                // Mando llamar el metodo de MYSQLConexion el cual me devuelve un datatable y lo serializo
+                json = JsonConvert.SerializeObject(conexion.consulta_busqueda(query));
+            }
+
+            //Retorno la cadena con formato json
+            return json;
+        }
+
     }
 }
